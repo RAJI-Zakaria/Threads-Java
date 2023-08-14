858aa20654da50ce51c0cacb248b144bf9787959
RAJI-Zakaria <zakariaraji.me@gmail.com>
2023-08-14 21:29:38 +0200

    adding real names to the philosophers program

--- Exercice-Philosophes/Main.java
+++ Exercice-Philosophes/Main.java
@@ -6,16 +6,16 @@ class Main {
     public static void main(String[] args) {
         int nbr_philo = 5;
         Semaphore[] forks = new Semaphore[nbr_philo];
-        Philosopher[] philos  = new Philosopher[nbr_philo];
-
+        Philosopher[] philos  = new Philosopher[nbr_philo]; 
+        
+        String[] names = {"Aristotle", "Plato", "Socrates", "Descartes", "Kant"};
 
         for(int i=0;i<nbr_philo;i++){
             forks[i] = new Semaphore(1);
         }
-
-
+ 
         for(int i=0;i<nbr_philo;i++){
-            philos[i] = new Philosopher(i+"",forks[i], forks[(i+1)%nbr_philo]);
+            philos[i] = new Philosopher(names[i],forks[i], forks[(i+1)%nbr_philo]);
             philos[i].start();
         }
 
